affdbe76a5e0e75290b50ae653e06ebb4dfc772a
Dan Cervelli <dcervelli>
2005-09-22 21:05:22 +0000

    Changed classpath.

--- swarm.bat
+++ swarm.bat
@@ -1,1 +1,2 @@
-start javaw -Xmx256M -cp .;lib/usgs.jar;lib/colt.jar;lib/seed-pdcc.jar gov.usgs.swarm.Swarm %1 %2 %3 %4 %5 %6 %7 %8 %9
+@echo off
+start javaw -Xmx256M -cp lib/usgs.jar;lib/colt.jar;lib/seed-pdcc.jar gov.usgs.swarm.Swarm %1 %2 %3 %4 %5 %6 %7 %8 %9
