d49a75d88ca0d39b0416cb5dbd60fdd55e270994
Dan Cervelli <dcervelli>
2006-04-15 16:05:01 +0000

    Added looks-2.0.1.jar to classpath.

--- swarm.bat
+++ swarm.bat
@@ -1,2 +1,2 @@
 @echo off
-start javaw -Xmx256M -cp .;lib/usgs.jar;lib/colt.jar;lib/seed-pdcc.jar gov.usgs.swarm.Swarm %1 %2 %3 %4 %5 %6 %7 %8 %9
+start javaw -Xmx256M -cp .;lib/usgs.jar;lib/colt.jar;lib/seed-pdcc.jar;lib/looks-2.0.1.jar gov.usgs.swarm.Swarm %1 %2 %3 %4 %5 %6 %7 %8 %9
