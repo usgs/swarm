ee43fa6d5fbabead9a58dd025fa6efeacb6259de
cervelli <cervelli>
2006-07-30 22:45:30 +0000

    Uses new gulper.

--- gov/usgs/swarm/data/WaveServerSource.java
+++ gov/usgs/swarm/data/WaveServerSource.java
@@ -20,6 +20,9 @@ import java.util.Map;
  * Earthworm Wave Server.
  * 
  * $Log: not supported by cvs2svn $
+ * Revision 1.5  2006/07/26 00:36:03  cervelli
+ * Changes for new gulper system.
+ *
  * Revision 1.4  2006/06/05 18:07:03  dcervelli
  * Major 1.3 changes.
  *
@@ -73,6 +76,7 @@ public class WaveServerSource extends SeismicDataSource
 	public WaveServerSource(WaveServerSource wss)
 	{
 		this(wss.params);
+		name = wss.name;
 	}
  
 	public SeismicDataSource getCopy()
@@ -182,161 +186,22 @@ public class WaveServerSource extends SeismicDataSource
 	
 	public synchronized HelicorderData getHelicorder(String station, double t1, double t2, GulperListener gl)
 	{
+		Thread.dumpStack();
 		double now = CurrentTime.getInstance().nowJ2K();
 		// if a time later than now has been asked for make sure to get the latest 
 		if ((t2 - now) >= -20)
 			getWave(station, now - 2*60, now);	
 		
 		CachedDataSource cache = Swarm.getCache();
-		HelicorderData hd = cache.getHelicorder(station, t1, t2, gl);	
+		HelicorderData hd = cache.getHelicorder(station, t1, t2, (GulperListener)null);	
 
-		if (hd == null || hd.rows() == 0 || (hd.getStartTime() - t1 > 0))
-			requestGulper(station, t1, t2);
+		if (hd == null || hd.rows() == 0 || (hd.getStartTime() - t1 > 10))
+			GulperList.getInstance().requestGulper("ws:" + station, gl, this, station, t1, t2);
 		return hd;
 	}
 	
-	public synchronized void notifyDataNotNeeded(String station, double t1, double t2)
+	public synchronized void notifyDataNotNeeded(String station, double t1, double t2, GulperListener gl)
 	{
-		stopGulper(station);
+		GulperList.getInstance().killGulper("ws:" + station, gl);
 	}
-	
-	private final static int MAX_GULPERS = 5;
-	private List<Gulper> gulpers;
-	
-	public synchronized boolean isActiveSource()
-	{
-		return (gulpers != null && gulpers.size() > 0);	
-	}
-	
-	public synchronized void requestGulper(String ch, double t1, double t2)
-	{
-		if (gulpers == null)
-			gulpers = new ArrayList<Gulper>();
-			
-		boolean allowed = true;
-//		for (int i = 0; i < gulpers.size(); i++)
-		for (Gulper g : gulpers)
-		{
-//			Gulper g = (Gulper)gulpers.elementAt(i);
-			if (g.channel == ch)	
-			{
-				allowed = false;
-				g.update(t1, t2);
-				break;
-			}
-		}
-		
-		if (gulpers.size() >= MAX_GULPERS)
-			allowed = false;
-		
-		if (allowed)
-		{
-			Gulper g = new Gulper(ch, t1, t2);
-			gulpers.add(g);
-		}
-	}
-
-	public synchronized void removeGulper(Gulper g)
-	{
-		gulpers.remove(g);	
-	}
-	
-	public synchronized void stopGulper(String ch)
-	{
-		if (gulpers == null)
-			return;
-		for (Gulper g : gulpers)
-		{
-			if (g.channel == ch)	
-			{
-				g.kill();
-				break;
-			}
-		}
-	}
-	
-	class Gulper extends Thread
-	{
-		private SeismicDataSource gulpSource;
-		private String channel;
-		private double lastTime;
-		private double goalTime;
-		private boolean killed;
-		
-		private int GULP_SIZE = 15 * 60;
-		private int WAIT_INTERVAL = 1 * 1000;
-		
-		public Gulper(String ch, double t1, double t2)
-		{
-			channel = ch;
-			lastTime = t2;
-			
-			double now = CurrentTime.getInstance().nowJ2K();
-			if (lastTime > now)
-				lastTime = now;
-			
-			update(t1, t2);
-			
-			gulpSource = new WaveServerSource(params);
-			((WaveServerSource)gulpSource).setTimeout(30*1000);
-			
-			start();
-			System.out.println("Gulper started for " + channel);
-		}
-		
-		public void kill()
-		{
-			killed = true;	
-			interrupt();
-		}
-		
-		public void update(double t1, double t2)
-		{
-			CachedDataSource cache = Swarm.getCache();
-			if (t2 < lastTime)
-				lastTime = t2;
-			goalTime = t1;
-
-			while (cache.inHelicorderCache(channel, lastTime - GULP_SIZE, lastTime) && lastTime > goalTime && !killed)
-			{
-				lastTime -= GULP_SIZE;
-				lastTime += 10;	
-			}
-		}
-		
-		public void run()
-		{
-			while (lastTime > goalTime && !killed)
-			{
-				try
-				{
-					if (gulpSource.getWave(channel, lastTime - GULP_SIZE, lastTime) != null)
-						update(goalTime, lastTime - GULP_SIZE + 10);
-				}
-				catch (Exception e)
-				{
-					System.err.println("Exception during gulp:");
-					e.printStackTrace();	
-				}
-				
-				if (!killed)
-					try { Thread.sleep(WAIT_INTERVAL); } catch (Exception e) {}
-			}	
-			
-			gulpSource.close();
-			
-			if (killed)
-				System.out.println("Gulper killed");
-			else
-				System.out.println("Gulper finished");
-				
-			removeGulper(this);
-		}
-		
-		public String toString()
-		{
-			return channel;	
-		}
-	}
-	
 }
