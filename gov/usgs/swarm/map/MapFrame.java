af1f25e5b3501899ef714d543a3f272615741d68
cervelli <cervelli>
2006-08-02 23:34:56 +0000

    Layout changes.

--- gov/usgs/swarm/map/MapFrame.java
+++ gov/usgs/swarm/map/MapFrame.java
@@ -25,7 +25,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -49,6 +48,9 @@ import javax.swing.event.InternalFrameEvent;
 
 /**
  * $Log: not supported by cvs2svn $
+ * Revision 1.8  2006/08/01 23:45:09  cervelli
+ * More development.
+ *
  * Revision 1.7  2006/07/30 22:47:04  cervelli
  * Changes for layouts.
  *
@@ -119,19 +121,13 @@ public class MapFrame extends SwarmFrame implements Runnable
 	public void saveLayout(ConfigFile cf, String prefix)
 	{
 		super.saveLayout(cf, prefix);
-		cf.put(prefix + ".longitude", Double.toString(mapPanel.getCenter().x));
-		cf.put(prefix + ".latitude", Double.toString(mapPanel.getCenter().y));
-		cf.put(prefix + ".scale", Double.toString(mapPanel.getScale()));
+		mapPanel.saveLayout(cf, prefix + ".panel");
 	}
 	
 	public void processLayout(ConfigFile cf)
 	{
 		processStandardLayout(cf);
-		double lon = Double.parseDouble(cf.getString("longitude"));
-		double lat = Double.parseDouble(cf.getString("latitude"));
-		Point2D.Double c = new Point2D.Double(lon, lat);
-		double scale = Double.parseDouble(cf.getString("scale"));
-		mapPanel.setCenterAndScale(c, scale);
+		mapPanel.processLayout(cf.getSubConfig("panel"));
 	}
 	
 	private void createUI()
@@ -562,7 +558,7 @@ public class MapFrame extends SwarmFrame implements Runnable
 				
 				Thread.sleep(refreshInterval);
 			}
-			catch (Exception e)
+			catch (Throwable e)
 			{
 				e.printStackTrace();
 			}
