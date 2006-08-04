c3a0b82cb93bc650f7f2cb2bec7306c509a40b5f
cervelli <cervelli>
2006-08-04 21:18:21 +0000

    Added CVS log.

--- gov/usgs/swarm/TimeListener.java
+++ gov/usgs/swarm/TimeListener.java
@@ -3,7 +3,7 @@ package gov.usgs.swarm;
 import java.util.EventListener;
 
 /**
- * 
+ * $Log: not supported by cvs2svn $
  * @author Dan Cervelli
  */
 public interface TimeListener extends EventListener
