41f8e568fab63decc280eaf1a477d2889d3f4563
Tom Parker <tparker@usgs.gov>
2014-12-31 20:23:53 +0000

    correct ConcurrentModificationException when closing all windows

--- gov/usgs/swarm/internalFrame/SwarmInternalFrames.java
+++ gov/usgs/swarm/internalFrame/SwarmInternalFrames.java
@@ -1,15 +1,12 @@
 package gov.usgs.swarm.internalFrame;
 
-import gov.usgs.swarm.SwarmConfig;
 import gov.usgs.swarm.heli.HelicorderViewerFrame;
-import gov.usgs.swarm.map.MapFrame;
-import gov.usgs.swarm.options.SwarmOptions;
-import gov.usgs.swarm.options.SwarmOptionsListener;
 import gov.usgs.swarm.wave.MultiMonitor;
 import gov.usgs.swarm.wave.WaveViewerFrame;
 
+import java.beans.PropertyVetoException;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.swing.JInternalFrame;
@@ -20,28 +17,31 @@ import javax.swing.event.EventListenerList;
  * A singleton ArrayList of internal frames which keeps a list of listeners.
  * 
  * @author Tom Parker
- *
+ * 
  */
 public final class SwarmInternalFrames {
 
     private static final EventListenerList internalFrameListeners = new EventListenerList();
     private static final ArrayList<JInternalFrame> internalFrames = new ArrayList<JInternalFrame>();
 
-    private SwarmInternalFrames () {
+    private SwarmInternalFrames() {
     }
 
     public static void removeAllFrames() {
         Runnable r = new Runnable() {
             public void run() {
-                Iterator<JInternalFrame> it = internalFrames.iterator();
-                while (it.hasNext()) {
-                    JInternalFrame frame = it.next();
+
+                List<JInternalFrame> toRemove = new LinkedList<JInternalFrame>();
+                for (JInternalFrame frame : internalFrames)
                     if (frame instanceof HelicorderViewerFrame || frame instanceof WaveViewerFrame
                             || frame instanceof MultiMonitor) {
-                        try {
-                            frame.setClosed(true);
-                        } catch (Exception e) {
-                        }
+                        toRemove.add(frame);
+                    }
+
+                for (JInternalFrame frame : toRemove) {
+                    try {
+                        frame.setClosed(true);
+                    } catch (PropertyVetoException ignoredException) {
                     }
                 }
             }
@@ -56,14 +56,14 @@ public final class SwarmInternalFrames {
             }
         }
     }
-    
+
     public static void remove(final JInternalFrame f) {
         for (InternalFrameListener listener : internalFrameListeners.getListeners(InternalFrameListener.class))
             listener.internalFrameRemoved(f);
-        
+
         internalFrames.remove(f);
     }
-    
+
     public static void add(final JInternalFrame f) {
         add(f, true);
     }
@@ -71,12 +71,12 @@ public final class SwarmInternalFrames {
     public static void add(final JInternalFrame f, boolean setLoc) {
         if (setLoc)
             f.setLocation(internalFrames.size() * 24, internalFrames.size() * 24);
-         
+
         for (InternalFrameListener listener : internalFrameListeners.getListeners(InternalFrameListener.class))
             listener.internalFrameAdded(f);
-        
+
         internalFrames.add(f);
-   }
+    }
 
     public static void addInternalFrameListener(InternalFrameListener tl) {
         internalFrameListeners.add(InternalFrameListener.class, tl);
@@ -85,11 +85,11 @@ public final class SwarmInternalFrames {
     public static void removeInternalFrameListener(InternalFrameListener tl) {
         internalFrameListeners.remove(InternalFrameListener.class, tl);
     }
-    
+
     public static int frameCount() {
         return internalFrames.size();
     }
-    
+
     public static List<JInternalFrame> getFrames() {
         return internalFrames;
     }
