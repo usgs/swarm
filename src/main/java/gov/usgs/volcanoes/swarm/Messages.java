a5d018ef77eb92a1b996b7d35f8d1f51def1187e
Tom Parker <tparker@usgs.gov>
2015-08-20 16:26:18 -0800

    Maven-ization complete?

--- src/main/java/gov/usgs/volcanoes/swarm/Messages.java
+++ src/main/java/gov/usgs/volcanoes/swarm/Messages.java
@@ -11,7 +11,7 @@ import java.util.ResourceBundle;
  */
 public class Messages
 {
-	private static final String BUNDLE_NAME = "gov.usgs.swarm.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, Locale.ENGLISH);
 
