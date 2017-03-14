a4bfa10a4bd327dda5ee0b0d19e25b97bd7dda61
Diana Norgaard <dnorgaard@usgs.gov>
2017-03-14 09:54:03 -0700

    Revert "Add button to allow data source refresh (issue #41) and code cleanup."
    This reverts commit 791869f74c0d859262e78cef8565f98220b2767a.

--- src/main/java/gov/usgs/volcanoes/swarm/Icons.java
+++ src/main/java/gov/usgs/volcanoes/swarm/Icons.java
@@ -30,7 +30,6 @@ public class Icons {
     public static final ImageIcon minimize = getIcon("images/minimize.png");
     public static final ImageIcon monitor = getIcon("images/monitor.gif");
     public static final ImageIcon open = getIcon("images/wave_folder.gif");
-    public static final ImageIcon refresh = getIcon("images/refresh.gif");
     public static final ImageIcon right = getIcon("images/right.gif");
     public static final ImageIcon save = getIcon("images/save.gif");
     public static final ImageIcon saveall = getIcon("images/saveall.gif");
