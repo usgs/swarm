260734f6958d6685ce8a74d126b44cc64b78ff58
Diana Norgaard <dnorgaard@usgs.gov>
2017-03-14 09:53:52 -0700

    Revert "commit with rest of changes from 791869f"
    This reverts commit 3e3b187fe1dd01f67db86268d5b556385b7cf21d.

--- src/main/java/gov/usgs/volcanoes/swarm/chooser/DataChooser.java
+++ src/main/java/gov/usgs/volcanoes/swarm/chooser/DataChooser.java
@@ -628,24 +628,14 @@ public class DataChooser extends JPanel {
 		add(bottomPanel, BorderLayout.SOUTH);
 	}
 
-	/**
-	 * @return
-	 */
 	public int getDividerLocation() {
 		return split.getDividerLocation();
 	}
 
-	/**
-	 * @param dl
-	 */
 	public void setDividerLocation(int dl) {
 		split.setDividerLocation(dl);
 	}
 
-	/**
-	 * @param svr
-	 * @return
-	 */
 	private ServerNode getServerNode(String svr) {
 		for (int i = 0; i < rootNode.getChildCount(); i++) {
 			ServerNode node = (ServerNode) rootNode.getChildAt(i);
@@ -656,9 +646,6 @@ public class DataChooser extends JPanel {
 		return null;
 	}
 
-	/**
-	 * @return
-	 */
 	private List<ServerNode> getSelectedServers() {
 		TreePath[] paths = dataTree.getSelectionPaths();
 		List<ServerNode> servers = new ArrayList<ServerNode>();
@@ -812,7 +799,6 @@ public class DataChooser extends JPanel {
 			 * @see gov.usgs.volcanoes.swarm.data.SeismicDataSourceListener#channelsUpdated()
 			 */
 			public void channelsUpdated() {
-				
 			}
 
 			/* (non-Javadoc)
