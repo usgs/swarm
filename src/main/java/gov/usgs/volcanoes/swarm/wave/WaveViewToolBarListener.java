e49bc4dcab778d46507c8134e611a2432fb24c58
Diana Norgaard <dnorgaard@usgs.gov>
2017-03-27 15:16:11 -0700

    Bring to compliance with Google Java code style.

--- src/main/java/gov/usgs/volcanoes/swarm/wave/WaveViewToolBarListener.java
+++ src/main/java/gov/usgs/volcanoes/swarm/wave/WaveViewToolBarListener.java
@@ -1,11 +1,12 @@
 package gov.usgs.volcanoes.swarm.wave;
 
-import javax.swing.AbstractButton;
-
 import gov.usgs.volcanoes.swarm.wave.WaveViewSettings.ViewType;
+import javax.swing.AbstractButton;
 
 public interface WaveViewToolBarListener {
   public void displaySettingsDialog();
+  
   public void mapKeyStroke(String keyStroke,String name, AbstractButton button);
+  
   public void setType(ViewType viewType);
 }
