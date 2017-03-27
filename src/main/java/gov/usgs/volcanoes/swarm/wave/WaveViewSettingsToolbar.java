d54b9987a90511ca0464733b5f9a5e8ee877d671
Diana Norgaard <dnorgaard@usgs.gov>
2017-03-27 15:16:11 -0700

    Bring to compliance with Google Java code style.

--- src/main/java/gov/usgs/volcanoes/swarm/wave/WaveViewSettingsToolbar.java
+++ src/main/java/gov/usgs/volcanoes/swarm/wave/WaveViewSettingsToolbar.java
@@ -19,214 +19,222 @@ import javax.swing.JToolBar;
 import javax.swing.KeyStroke;
 
 /**
- * 
+ * Wave view settings toolbar.
  *
  * @author Dan Cervelli
  */
-public class WaveViewSettingsToolbar
-{
-	private JButton waveSet;
-	private JToggleButton waveToggle;
-	private JToggleButton spectraToggle;
-	private JToggleButton spectrogramToggle;
-	private ButtonGroup waveTypes;
-	
-	private Set<WaveViewSettings> settingsSet;
-	
-	public WaveViewSettingsToolbar(WaveViewSettings s, JToolBar dest, JComponent keyComp)
-	{
-		settingsSet = new HashSet<WaveViewSettings>();
-		createUI(dest, keyComp);
-		setSettings(s);
-	}
-	
-	public void createUI(JToolBar dest, JComponent keyComp)
-	{
-		waveSet = SwarmUtil.createToolBarButton(
-				Icons.wavesettings,
-				"Wave view settings (?)",
-				new ActionListener()
-				{
-					public void actionPerformed(ActionEvent e)
-					{
-						if (settingsSet.size()==0)
-							return;
-						WaveViewSettings s = settingsSet.iterator().next();
-						WaveViewSettingsDialog wvsd = WaveViewSettingsDialog.getInstance(s, settingsSet.size());
-						wvsd.setVisible(true);
-						for (WaveViewSettings settings : settingsSet)
-						{
-							settings.copy(s);
-							settings.notifyView();
-						}
-					}
-				});
-		UiUtils.mapKeyStrokeToButton(keyComp, "shift SLASH", "settings", waveSet);
-		dest.add(waveSet);
-		
-		waveToggle = SwarmUtil.createToolBarToggleButton(
-				Icons.wave,
-				"Wave view (W or ,)",
-				new ActionListener()
-				{
-					public void actionPerformed(ActionEvent e)
-					{
-						for (WaveViewSettings settings : settingsSet)
-							settings.setType(ViewType.WAVE);
-					}
-				});
-		UiUtils.mapKeyStrokeToButton(keyComp, "COMMA", "wave1", waveToggle);
-		UiUtils.mapKeyStrokeToButton(keyComp, "W", "wave2", waveToggle);
-		dest.add(waveToggle);
-		
-		spectraToggle = SwarmUtil.createToolBarToggleButton(
-				Icons.spectra,
-				"Spectra view (S or .)",
-				new ActionListener()
-				{
-					public void actionPerformed(ActionEvent e)
-					{
-						for (WaveViewSettings settings : settingsSet)
-							settings.setType(ViewType.SPECTRA);
-					}
-				});
-		UiUtils.mapKeyStrokeToButton(keyComp, "PERIOD", "spectra1", spectraToggle);
-		UiUtils.mapKeyStrokeToButton(keyComp, "S", "spectra2", spectraToggle);
-		dest.add(spectraToggle);
-		
-		spectrogramToggle = SwarmUtil.createToolBarToggleButton(
-				Icons.spectrogram,
-				"Spectrogram view (G or /)",
-				new ActionListener()
-				{
-					public void actionPerformed(ActionEvent e)
-					{
-						for (WaveViewSettings settings : settingsSet)							
-							settings.setType(ViewType.SPECTROGRAM);
-					}
-				});
-		UiUtils.mapKeyStrokeToButton(keyComp, "SLASH", "spectrogram1", spectrogramToggle);
-		UiUtils.mapKeyStrokeToButton(keyComp, "G", "spectrogram2", spectrogramToggle);
-		dest.add(spectrogramToggle);
-		
-		keyComp.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(KeyStroke.getKeyStroke("L"), "cycleLogSettings");
-		keyComp.getActionMap().put("cycleLogSettings", new AbstractAction()
-				{
-					public static final long serialVersionUID = -1;
-					public void actionPerformed(ActionEvent e)
-					{
-						for (WaveViewSettings settings : settingsSet) {
-							if (settings.viewType == ViewType.SPECTRA)
-							settings.cycleLogSettings();
-							if (settings.viewType == ViewType.SPECTROGRAM)
-								settings.toggleLogPower();
-						}
-					}	
-				});
-				
-		keyComp.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(KeyStroke.getKeyStroke("F"), "toggleFilter");
-		keyComp.getActionMap().put("toggleFilter", new AbstractAction()
-				{
-					public static final long serialVersionUID = -1;
-					public void actionPerformed(ActionEvent e)
-					{
-						for (WaveViewSettings settings : settingsSet)
-							settings.toggleFilter();
-					}	
-				});				
-				
-		keyComp.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(KeyStroke.getKeyStroke("R"), "resetAutoScale");
-		keyComp.getActionMap().put("resetAutoScale", new AbstractAction()
-				{
-					public static final long serialVersionUID = -1;
-					public void actionPerformed(ActionEvent e)
-					{
-						for (WaveViewSettings settings : settingsSet)
-							settings.resetAutoScaleMemory();
-					}	
-				});
-		
-		keyComp.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(KeyStroke.getKeyStroke("OPEN_BRACKET"), "yScaleIn");
-		keyComp.getActionMap().put("yScaleIn", new AbstractAction()
-				{
-					public static final long serialVersionUID = -1;
-					public void actionPerformed(ActionEvent e)
-					{
-						for (WaveViewSettings settings : settingsSet) {
-							settings.adjustScale(1.0 / 1.25);
-						}
-					}	
-				});
-		
-		keyComp.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(KeyStroke.getKeyStroke("CLOSE_BRACKET"), "yScaleOut");
-		keyComp.getActionMap().put("yScaleOut", new AbstractAction()
-				{
-					public static final long serialVersionUID = -1;
-					public void actionPerformed(ActionEvent e)
-					{
-						for (WaveViewSettings settings : settingsSet)
-							settings.adjustScale(1.25);
-					}	
-				});
-				
-		waveTypes = new ButtonGroup();
-		waveTypes.add(waveToggle);
-		waveTypes.add(spectraToggle);
-		waveTypes.add(spectrogramToggle);
-	}
-	
-	public void clearSettingsSet()
-	{
-		settingsSet.clear();
-	}
-	
-	public void addSettings(WaveViewSettings s)
-	{
-		if (s != null)
-		{
-			settingsSet.add(s);
-			s.toolbar = this;
-			settingsChanged();
-		}
-	}
-	
-	public void removeSettings(WaveViewSettings s)
-	{
-		settingsSet.remove(s);
-		if (s != null)
-		{
-			s.toolbar = null;
-			settingsChanged();
-		}
-	}
-	
-	public void setSettings(WaveViewSettings s)
-	{
-		clearSettingsSet();
-		addSettings(s);
-	}
-	
-	public void settingsChanged()
-	{
-		boolean w = false;
-		boolean s = false;
-		boolean sg = false;
-		for (WaveViewSettings set : settingsSet)
-		{
-			if (set.viewType == ViewType.WAVE)
-				w = true;
-			if (set.viewType == ViewType.SPECTRA)
-				s = true;
-			if (set.viewType == ViewType.SPECTROGRAM)
-				sg = true;
-		}
+public class WaveViewSettingsToolbar {
+  private JButton waveSet;
+  private JToggleButton waveToggle;
+  private JToggleButton spectraToggle;
+  private JToggleButton spectrogramToggle;
+  private ButtonGroup waveTypes;
 
-		// fix for Java 1.5, clearSelection was added in 1.6
-		try { 
-			waveTypes.setSelected( waveTypes.getSelection(), false );
-		} catch (Throwable e) {}
-		waveToggle.setSelected(w && !s && !sg);	
-		spectraToggle.setSelected(!w && s && !sg);
-		spectrogramToggle.setSelected(!w && !s && sg);
-	}
+  private Set<WaveViewSettings> settingsSet;
+
+  /**
+   * Constructor.
+   * @param s  Wave view settings
+   * @param dest Tool bar destination
+   * @param keyComp key component
+   */
+  public WaveViewSettingsToolbar(WaveViewSettings s, JToolBar dest, JComponent keyComp) {
+    settingsSet = new HashSet<WaveViewSettings>();
+    createUi(dest, keyComp);
+    setSettings(s);
+  }
+
+  /**
+   * Create wave view settings tool bar.
+   * @param dest tool bar
+   * @param keyComp key component
+   */
+  public void createUi(JToolBar dest, JComponent keyComp) {
+    waveSet = SwarmUtil.createToolBarButton(Icons.wavesettings, "Wave view settings (?)",
+        new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            if (settingsSet.size() == 0) {
+              return;
+            }
+            WaveViewSettings s = settingsSet.iterator().next();
+            WaveViewSettingsDialog wvsd = WaveViewSettingsDialog.getInstance(s, settingsSet.size());
+            wvsd.setVisible(true);
+            for (WaveViewSettings settings : settingsSet) {
+              settings.copy(s);
+              settings.notifyView();
+            }
+          }
+        });
+    UiUtils.mapKeyStrokeToButton(keyComp, "shift SLASH", "settings", waveSet);
+    dest.add(waveSet);
+
+    waveToggle =
+        SwarmUtil.createToolBarToggleButton(Icons.wave, "Wave view (W or ,)", new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            for (WaveViewSettings settings : settingsSet) {
+              settings.setType(ViewType.WAVE);
+            }
+          }
+        });
+    UiUtils.mapKeyStrokeToButton(keyComp, "COMMA", "wave1", waveToggle);
+    UiUtils.mapKeyStrokeToButton(keyComp, "W", "wave2", waveToggle);
+    dest.add(waveToggle);
+
+    spectraToggle = SwarmUtil.createToolBarToggleButton(Icons.spectra, "Spectra view (S or .)",
+        new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            for (WaveViewSettings settings : settingsSet) {
+              settings.setType(ViewType.SPECTRA);
+            }
+          }
+        });
+    UiUtils.mapKeyStrokeToButton(keyComp, "PERIOD", "spectra1", spectraToggle);
+    UiUtils.mapKeyStrokeToButton(keyComp, "S", "spectra2", spectraToggle);
+    dest.add(spectraToggle);
+
+    spectrogramToggle = SwarmUtil.createToolBarToggleButton(Icons.spectrogram,
+        "Spectrogram view (G or /)", new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            for (WaveViewSettings settings : settingsSet) {
+              settings.setType(ViewType.SPECTROGRAM);
+            }
+          }
+        });
+    UiUtils.mapKeyStrokeToButton(keyComp, "SLASH", "spectrogram1", spectrogramToggle);
+    UiUtils.mapKeyStrokeToButton(keyComp, "G", "spectrogram2", spectrogramToggle);
+    dest.add(spectrogramToggle);
+
+    keyComp.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT)
+        .put(KeyStroke.getKeyStroke("L"), "cycleLogSettings");
+    keyComp.getActionMap().put("cycleLogSettings", new AbstractAction() {
+      public static final long serialVersionUID = -1;
+
+      public void actionPerformed(ActionEvent e) {
+        for (WaveViewSettings settings : settingsSet) {
+          if (settings.viewType == ViewType.SPECTRA) {
+            settings.cycleLogSettings();
+          }
+          if (settings.viewType == ViewType.SPECTROGRAM) {
+            settings.toggleLogPower();
+          }
+        }
+      }
+    });
+
+    keyComp.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT)
+        .put(KeyStroke.getKeyStroke("F"), "toggleFilter");
+    keyComp.getActionMap().put("toggleFilter", new AbstractAction() {
+      public static final long serialVersionUID = -1;
+
+      public void actionPerformed(ActionEvent e) {
+        for (WaveViewSettings settings : settingsSet) {
+          settings.toggleFilter();
+        }
+      }
+    });
+
+    keyComp.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT)
+        .put(KeyStroke.getKeyStroke("R"), "resetAutoScale");
+    keyComp.getActionMap().put("resetAutoScale", new AbstractAction() {
+      public static final long serialVersionUID = -1;
+
+      public void actionPerformed(ActionEvent e) {
+        for (WaveViewSettings settings : settingsSet) {
+          settings.resetAutoScaleMemory();
+        }
+      }
+    });
+
+    keyComp.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT)
+        .put(KeyStroke.getKeyStroke("OPEN_BRACKET"), "yScaleIn");
+    keyComp.getActionMap().put("yScaleIn", new AbstractAction() {
+      public static final long serialVersionUID = -1;
+
+      public void actionPerformed(ActionEvent e) {
+        for (WaveViewSettings settings : settingsSet) {
+          settings.adjustScale(1.0 / 1.25);
+        }
+      }
+    });
+
+    keyComp.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT)
+        .put(KeyStroke.getKeyStroke("CLOSE_BRACKET"), "yScaleOut");
+    keyComp.getActionMap().put("yScaleOut", new AbstractAction() {
+      public static final long serialVersionUID = -1;
+
+      public void actionPerformed(ActionEvent e) {
+        for (WaveViewSettings settings : settingsSet) {
+          settings.adjustScale(1.25);
+        }
+      }
+    });
+
+    waveTypes = new ButtonGroup();
+    waveTypes.add(waveToggle);
+    waveTypes.add(spectraToggle);
+    waveTypes.add(spectrogramToggle);
+  }
+
+  public void clearSettingsSet() {
+    settingsSet.clear();
+  }
+
+  /**
+   * Add wave view settings.
+   * @param s settings
+   */
+  public void addSettings(WaveViewSettings s) {
+    if (s != null) {
+      settingsSet.add(s);
+      s.toolbar = this;
+      settingsChanged();
+    }
+  }
+
+  /**
+   * Remove wave view settings.
+   * @param s settings
+   */
+  public void removeSettings(WaveViewSettings s) {
+    settingsSet.remove(s);
+    if (s != null) {
+      s.toolbar = null;
+      settingsChanged();
+    }
+  }
+
+  public void setSettings(WaveViewSettings s) {
+    clearSettingsSet();
+    addSettings(s);
+  }
+
+  /**
+   * Process settings change.
+   */
+  public void settingsChanged() {
+    boolean w = false;
+    boolean s = false;
+    boolean sg = false;
+    for (WaveViewSettings set : settingsSet) {
+      if (set.viewType == ViewType.WAVE) {
+        w = true;
+      }
+      if (set.viewType == ViewType.SPECTRA) {
+        s = true;
+      }
+      if (set.viewType == ViewType.SPECTROGRAM) {
+        sg = true;
+      }
+    }
+
+    // fix for Java 1.5, clearSelection was added in 1.6
+    try {
+      waveTypes.setSelected(waveTypes.getSelection(), false);
+    } catch (Throwable e) {
+      // what should happen here?
+    }
+    waveToggle.setSelected(w && !s && !sg);
+    spectraToggle.setSelected(!w && s && !sg);
+    spectrogramToggle.setSelected(!w && !s && sg);
+  }
 }
