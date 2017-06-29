3e9fd427926f4b01a4d5308415aa2a209102b0d5
Diana Norgaard <dnorgaard@usgs.gov>
2017-06-29 13:56:47 -0700

    handle null origin; replace use of deprecated method

--- src/main/java/gov/usgs/volcanoes/swarm/map/hypocenters/HypocenterLayer.java
+++ src/main/java/gov/usgs/volcanoes/swarm/map/hypocenters/HypocenterLayer.java
@@ -114,7 +114,9 @@ public final class HypocenterLayer implements MapLayer, ConfigListener, QuakemlO
 
     for (final Event event : events.values()) {
       Origin origin = event.getPreferredOrigin();
-
+      if (origin == null) {
+        continue;
+      }
       Point2D.Double originLoc = new Point2D.Double(origin.getLongitude(), origin.getLatitude());
       if (!range.contains(originLoc)) {
         continue;
@@ -132,12 +134,10 @@ public final class HypocenterLayer implements MapLayer, ConfigListener, QuakemlO
       g2.translate(res.x, res.y);
 
       double mag = event.getPreferredMagnitude().getMag();
-      float diameter;
-
-      diameter = getMarkerSize(mag);
+      float diameter = getMarkerSize(mag);
+      renderer.shape = new Ellipse2D.Float(0f, 0f, diameter, diameter);
 
       long age = J2kSec.asEpoch(J2kSec.now()) - origin.getTime();
-      renderer.shape = new Ellipse2D.Float(0f, 0f, diameter, diameter);
       Color markerColor;
       if (event == hoverEvent) {
         markerColor = GREEN;
@@ -250,7 +250,7 @@ public final class HypocenterLayer implements MapLayer, ConfigListener, QuakemlO
     boolean handled = false;
 
     if (hoverEvent != null) {
-      LOGGER.debug("Opening event {}", hoverEvent.getEvid());
+      LOGGER.debug("Opening event {}", hoverEvent.getEventId());
       Swarm.openEvent(hoverEvent);
       handled = true;
       hoverEvent = null;
