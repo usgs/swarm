984f49dfa33631e4bc28886efe2f0595a55dfda8
Diana Norgaard <dnorgaard@usgs.gov>
2020-02-20 09:59:23 -0800

    deletions of files no longer used or maintained

---
+++ src/main/java/gov/usgs/volcanoes/swarm/Version.java
@@ -0,0 +1,10 @@
+package gov.usgs.volcanoes.swarm;
+
+public final class Version {
+
+    public static final String BUILD_TIME="2020-02-20T17:52:54Z";
+
+    public static final String POM_VERSION="3.1.0";
+
+	public static final String VERSION_STRING = "Version: " + POM_VERSION + " Built: " + BUILD_TIME;
+}
